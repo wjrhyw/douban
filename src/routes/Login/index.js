b5de1526ccc9ba1a0c1ae5dc83a5ab494c662efd
root <root@LAPTOP-7MC2NSBI.localdomain>
2019-11-21 10:02:20 +0800

    wjr

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -35,7 +35,7 @@ export default class Login extends React.Component{
         form.validateFields((err, values) => {
             console.log('err是',err);
             if (!err) {
-              console.log(values);
+              console.log('wwwww',values);
             }
           });
         if(username.value!='wh'||password.value!=123){
